d9e8501f94e7c9d1985a6b4527dcc068f6952063
jelenatabak <jelena.tabak@gmail.com>
2022-08-29 14:58:18 +0200

    add states

--- ros2_semantic_segmentation/segment.py
+++ ros2_semantic_segmentation/segment.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 import numpy as np
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
 
 import cv2
 from cv_bridge import CvBridge
@@ -13,33 +14,36 @@ class SemanticSegmentation(Node):
 
     def __init__(self):
         super().__init__('semantic_segmentation')
-        self.subscription = self.create_subscription(Image, '/usv/arm/wrist/image_raw', self.image_callback, 1)
+        self.subscription = self.create_subscription(String, '/uav1/state', self.state_callback, 1)
+        self.subscription = self.create_subscription(Image, '/uav1/slot3/image_raw', self.image_callback, 1)
         self.subscription  # prevent unused variable warning
+
+        self.state = "IDLE"
 
         self.publisher = self.create_publisher(Image, '/segmentation_mask', 1)
         self.bridge = CvBridge()
 
-        self.model_path = '/home/developer/mbzirc_ws/src/ros2_semantic_segmentation/models/zdenka_model_suction'
+        self.model_path = '/home/developer/mbzirc_ws/src/ros2_semantic_segmentation/models/scenario_model'
         self.deeplab_predict = DeeplabInference(self.model_path, ros_structure=True)
         self.get_logger().info('Model loaded')
 
+    def state_callback(self, msg):
+        self.state = msg.data
+        self.get_logger().info(self.state)
+
     def image_callback(self, msg):
-        self.get_logger().info('New msg')
-
-        img = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
-        img = img.astype("float32")
-        img = img[:,80:560,:]
-
-        # cv2.imshow('image', img)
-        # cv2.waitKey(0) 
-        # cv2.destroyAllWindows() 
-        
-        mask = self.deeplab_predict.predict(img)
-        mask = mask.astype(np.uint8)
-        mask_msg = self.bridge.cv2_to_imgmsg(mask, 'rgb8')
-        mask_msg.header = msg.header
-        self.publisher.publish(mask_msg)
-        self.get_logger().info('Published')
+        if self.state =="SEARCH" or self.state == "SERVO":
+            self.get_logger().info('New msg')
+            img = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
+            img = img.astype("float32")
+            img = img[:,80:560,:]
+            
+            mask = self.deeplab_predict.predict(img)
+            mask = mask.astype(np.uint8)
+            mask_msg = self.bridge.cv2_to_imgmsg(mask, 'rgb8')
+            mask_msg.header = msg.header
+            self.publisher.publish(mask_msg)
+            self.get_logger().info('Published segmentation mask')
 
 
 def main(args=None):
