9f11991a42a6201daba40bde3cab69b97ca1e88d
jelenatabak <jelena.tabak@gmail.com>
2022-08-30 07:59:57 +0200

    report targets

--- ros2_semantic_segmentation/mbzirc_semantic_segmentation.py
+++ ros2_semantic_segmentation/mbzirc_semantic_segmentation.py
@@ -20,8 +20,10 @@ class SemanticSegmentation(Node):
     def __init__(self):
         super().__init__('semantic_segmentation')
         self.state_sub_ = self.create_subscription(String, '/uav1/state', self.state_callback, 1)
+        self.status_sub_ = self.create_subscription(String, '/mbzirc/target/stream/status', self.status_callback, 1)
         self.image_sub_ = self.create_subscription(Image, '/uav1/slot3/image_raw', self.image_callback, 1) # TODO: message filters for time synchronization
 
+        self.state_pub_ = self.create_publisher(String, '/uav1/state', 1)
         self.seg_mask_pub_ = self.create_publisher(Image, '/segmentation_mask', 1)
         self.centroid_img_pub_ = self.create_publisher(Image, '/centroid_tracker/detected_centroids', 1)
         self.report_pub_ = self.create_publisher(StringVec, '/uav1/target_report', 1)
@@ -39,6 +41,7 @@ class SemanticSegmentation(Node):
         self.counter = 0
         self.small_target_identified = False
         self.targets_identified = False
+        self.waiting_for_response = False
 
         self.color_codes =	{
             1: (255,255,255),
@@ -58,11 +61,22 @@ class SemanticSegmentation(Node):
         self.trackers = [CentroidTracker() for i in range(5)]
 
     def state_callback(self, msg):
+        print("New state: " + msg.data)
         self.state = msg.data
+
+    def status_callback(self, msg):
+        print("Recieved target report status: " + msg.data)
+        self.waiting_for_response = False
+        if msg.data == 'small_object_id_success':
+            self.small_target_identified = True
+        elif msg.data == 'large_object_id_success':
+            self.targets_identified = True
+            state_msg = String()
+            state_msg.data = 'SERVO'
+            self.state_pub_.publish(state_msg)
 
     def image_callback(self, msg):
         if self.state =="SEARCH" or self.state == "SERVO":
-            self.get_logger().info('New msg')
             img = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
             img = img.astype("float32")
 
@@ -76,25 +90,23 @@ class SemanticSegmentation(Node):
             mask_msg = self.bridge.cv2_to_imgmsg(self.seg_mask, 'rgb8')
             mask_msg.header = msg.header
             self.seg_mask_pub_.publish(mask_msg)
-            self.get_logger().info('Published segmentation mask')
 
             self.counter += 1
 
-            # find centroids
+            # track centroids
             if self.state == 'SEARCH':
                 if not self.targets_identified:
                     for i in range(5):
                         mask = np.where(np.all(self.seg_mask == self.color_codes[i+1], axis=-1, keepdims=True), [255, 255, 255], [0, 0, 0])
                         mask = mask[:,:,0].astype(np.uint8)
 
-                        #find contours
+                        # find contours
                         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
                         max_cnt = []
                         centroids = []
 
                         if len(contours) != 0:
-                            self.get_logger().info('Contours found')
-                            #cv2.drawContours(self.seg_mask, contours, -1, (255,255,255), 3)
+                            # cv2.drawContours(self.seg_mask, contours, -1, (255,255,255), 3)
 
                             c_sorted = sorted(contours, key=cv2.contourArea)
                             max_cnt.append(c_sorted[-1])
@@ -104,7 +116,7 @@ class SemanticSegmentation(Node):
                         for c in max_cnt:
                             if cv2.contourArea(c) > 30:
                                 x,y,w,h = cv2.boundingRect(c)
-                                #cv2.rectangle(self.seg_mask,(x,y),(x+w,y+h),(0,255,0),2)
+                                # cv2.rectangle(self.seg_mask,(x,y),(x+w,y+h),(0,255,0),2)
 
                                 moments = cv2.moments(c)
                                 if moments["m00"] > 0:
@@ -123,20 +135,38 @@ class SemanticSegmentation(Node):
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
                             cv2.circle(self.seg_mask, (centroid[0], centroid[1]), 4, (255, 255, 255), -1)
 
+                    if not self.waiting_for_response and not self.small_target_identified:
+                        for i in range(3,5):
+                            if self.trackers[i].confirmed:
+                                self.small_target_centroid = self.trackers[i].confirmedCentroid
+
+                                print("Reporting object " + self.object_ids[i+1])
+                                cv2.circle(self.seg_mask, (self.small_target_centroid[0], self.small_target_centroid[1]), 7, (0, 255, 0), -1)
+
+                                report = StringVec()
+                                report.data = ['small', str(int(self.small_target_centroid[0])), str(int(self.small_target_centroid[1]))]
+                                self.report_pub_.publish(report)
+                                self.waiting_for_response = True
+
+                    if not self.waiting_for_response and self.small_target_identified:
+                        for i in range(3):
+                            if self.trackers[i].confirmed:
+                                self.large_target_centroid = self.trackers[i].confirmedCentroid
+
+                                print("Reporting object " + self.object_ids[i+1])
+                                cv2.circle(self.seg_mask, (self.small_target_centroid[0], self.small_target_centroid[1]), 7, (0, 255, 0), -1)
+
+                                report = StringVec()
+                                report.data = ['large', str(int(self.large_target_centroid[0])), str(int(self.large_target_centroid[1]))]
+                                self.report_pub_.publish(report)
+                                self.waiting_for_response = True
+
                     mask_msg = self.bridge.cv2_to_imgmsg(self.seg_mask, 'rgb8')
                     self.centroid_img_pub_.publish(mask_msg)
 
-
-                    if not self.small_target_identified:
-                        pass
-                        # trackers 0 i 1, salji operateru, wait for msg, postavi zastavicu, postavi poziciju plavog
-                    
-                    # plavi potvrdjen
-                    #  i crni bilo koji detektiran 2 puta: salji operateru, wait for msg, postavi zastavicu
-
             elif self.state == 'SERVO':
                 pass
-                # objavljujes centroid malog plavog (maska na onaj koji je detektiran + pozicija smije odstupati za delta)
+                # pocetni centroid imas, od novih detektiranih velicine iznad nekog delta, objavi poziciju onog blizeg prethodnom
 
     def pointcloud_callback():
         pass
